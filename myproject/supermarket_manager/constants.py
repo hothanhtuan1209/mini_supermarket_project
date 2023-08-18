ba2b33eb53da539161f25b668d66fb60bf15c2e8
Tuan <hothanhtuan1209@gmail.com>
2023-08-18 16:59:45 +0700

    Add constants.py file

---
+++ myproject/supermarket_manager/constants.py
@@ -0,0 +1,10 @@
+# Constant for status
+STATUS_SUCCESS = 200
+STATUS_BAD_REQUEST = 400
+STATUS_METHOD_NOT_ALLOWED = 405
+
+# Constant for message
+MESSAGE_ADDED = "Added successfully"
+MESSAGE_EXISTS = "Name already exists"
+MESSAGE_REQUIRED = "Name is required"
+MESSAGE_INVALID_METHOD = "Invalid request method"
