94e8bde039c181bf487e29a2305fbe33d46417e2
Tuan <hothanhtuan1209@gmail.com>
2023-08-21 10:16:19 +0700

    Add urls for views update and list

--- myproject/supermarket_manager/urls.py
+++ myproject/supermarket_manager/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from .views import add_role
+from .views import add_role, list_roles, update_role
 
-urlpatterns = [path("api/roles/add", add_role, name="add-role")]
+urlpatterns = [path("api/roles/add", add_role, name="add-role"),
+               path('api/roles/list_roles', list_roles, name='list-roles'),
+               path("api/roles/update/<str:role_id>", update_role, name="update-role")
+]
