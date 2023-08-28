194fd6fb03686353901eda21927d667e3ef01415
Tuan <hothanhtuan1209@gmail.com>
2023-08-28 09:21:49 +0700

    Update urls for feature get roles

--- myproject/supermarket_manager/urls.py
+++ myproject/supermarket_manager/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from .views import (
     add_role,
-    list_roles,
+    get_roles,
     update_role,
     delete_role,
 )
 
 urlpatterns = [
     path("api/roles/add", add_role, name="add-role"),
-    path("api/roles/list_roles", list_roles, name="list-roles"),
+    path("api/roles/list_roles", get_roles, name="get-roles"),
     path("api/roles/update/<str:role_id>", update_role, name="update-role"),
     path("api/roles/<str:role_id>", delete_role, name="delete-role"),
 ]
