0309285afa8cb2c0c5c847476d610a0fd31c3cd1
Tuan <hothanhtuan1209@gmail.com>
2023-08-28 09:51:59 +0700

    Update format URLs according to REST API

--- myproject/supermarket_manager/urls.py
+++ myproject/supermarket_manager/urls.py
@@ -7,8 +7,8 @@ from .views import (
 )
 
 urlpatterns = [
-    path("api/roles/add", add_role, name="add-role"),
-    path("api/roles/list_roles", get_roles, name="get-roles"),
-    path("api/roles/update/<str:role_id>", update_role, name="update-role"),
+    path("api/roles/adds", add_role, name="add-role"),
+    path("api/roles/get_roles", get_roles, name="get-roles"),
+    path("api/roles/updates/<str:role_id>", update_role, name="update-role"),
     path("api/roles/<str:role_id>", delete_role, name="delete-role"),
 ]
