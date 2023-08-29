dbc0a235fe56397b095c4c2421cbaaee16279139
Tuan <hothanhtuan1209@gmail.com>
2023-08-29 13:53:32 +0700

    Add url for toggle status feature

--- myproject/supermarket_manager/urls.py
+++ myproject/supermarket_manager/urls.py
@@ -17,4 +17,5 @@ urlpatterns = [
     path("api/permissions/adds", add_permission, name="add-permission"),
     path('api/permissions/get_permission', get_permissions, name='get-permission'),
     path("api/permission/updates/<str:permission_id>", update_permission, name="update-permission"),
+    path("api/permission/toggle-status/<str:permission_id>"),
 ]
