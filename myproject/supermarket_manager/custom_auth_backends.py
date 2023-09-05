e6dfe3ce35c2b72c58738864f4095ea806184946
Tuan <hothanhtuan1209@gmail.com>
2023-09-05 15:15:28 +0700

    Add file custom authenticate

---
+++ myproject/supermarket_manager/custom_auth_backends.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth.hashers import check_password
+
+from .models import Account
+
+
+class EmailBackend(ModelBackend):
+    """
+    Custom authentication backend for email-based authentication.
+
+    This backend allows users to authenticate using their email address
+    and password, instead of the default username and password.
+    """
+    def authenticate(self, request, email=None, password=None, **kwargs):
+        try:
+            user = Account.objects.get(email=email)
+            if user.check_password(password):
+                return user
+        except Account.DoesNotExist:
+            return None
+
+    def get_user(self, user_id):
+        try:
+            return Account.objects.get(pk=user_id)
+        except Account.DoesNotExist:
+            return None
