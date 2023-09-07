df695614a212f70c9719d734ed0edd98ffa2afa7
Tuan <hothanhtuan1209@gmail.com>
2023-09-07 16:32:33 +0700

    Add empty line in line 68 file model.py

--- myproject/supermarket_manager/models.py
+++ myproject/supermarket_manager/models.py
@@ -65,6 +65,7 @@ class AccountManager(BaseUserManager):
     def create_user(self, email, user_name, password=None, **extra_fields):
         if not email:
             raise ValueError('The Email field must be set')
+        
         email = self.normalize_email(email)
         user = self.model(email=email, user_name=user_name, **extra_fields)
         user.set_password(password)
