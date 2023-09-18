e84ee65269d228493c1a45aaeda20297003ef14f
Tuan <hothanhtuan1209@gmail.com>
2023-09-18 22:42:11 +0700

    Update endpoint create views

--- myproject/supermarket_manager/urls.py
+++ myproject/supermarket_manager/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from .views import (
-    add_role,
+    create_role,
     get_roles,
     update_role,
     delete_role,
-    add_permission,
+    create_permission,
     get_permissions,
     update_permission,
     assign_permission,
-    add_account,
+    create_account,
     get_account_detail,
     login_account,
     update_account,
@@ -16,15 +16,15 @@ from .views import (
 )
 
 urlpatterns = [
-    path("api/roles/adds", add_role, name="add-role"),
+    path("api/roles/create", create_role, name="create-role"),
     path("api/roles/get_roles", get_roles, name="get-roles"),
     path("api/roles/updates/<str:role_id>", update_role, name="update-role"),
     path("api/roles/<str:role_id>", delete_role, name="delete-role"),
-    path("api/permissions/adds", add_permission, name="add-permission"),
+    path("api/permissions/creates", create_permission, name="create-permission"),
     path('api/permissions/get_permission', get_permissions, name='get-permission'),
     path("api/permissions/updates/<str:permission_id>", update_permission, name="update-permission"),
     path("api/assign-permissions", assign_permission, name="assign-permission"),
-    path("api/accounts", add_account, name="add-account"),
+    path("api/accounts", create_account, name="create-account"),
     path("api/accounts/<str:account_id>", get_account_detail, name="get-account-detail"),
     path("api/logins", login_account, name="login"),
     path("api/accounts/updates/<str:account_id>", update_account, name="update-account"),
